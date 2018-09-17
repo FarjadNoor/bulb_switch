d1b82421d440ef88d45256d4c7b8b86b0872a2df
farjadnoor <mnoor.farjad@yahoo.com>
2018-09-17 23:40:17 +0500

    quiz app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,165 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import QuizInfo from './Screens/QuizInfo/QuizInfo'
+import QuizPage from './Screens/QuizPage/QuizPage'
+import Login from './Screens/Login/Login'
+import CourseList from './Screens/CourseList/CourseList'
+import KeyPage from './Screens/KeyPage/KeyPage'
 import './App.css';
-import Brokenbulb from './screens/Brokenbulb/Brokenbulb'
-import Bulb from './screens/Bulb/Bulb'
-import Lightbulb from './screens/Lightbulb/Lightbulb'
 
 class App extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
-      bulb: false,
-      switch: true
-    }
+      isLogin: false,
+      quizzes: [
+        {
+          name: 'AngularJs',
+          Quiz: [
+            {
+              name: 'quiz1', questions: [
+                {
+                  ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'], correct: 1 }],
+                  ques2: [{ ques: 'what is my name?', ans: ['Farjad', 'Noor', 'Eusufi'], correct: 1 }]
+                }
+              ]
+            },
+            {
+              name: 'quiz2', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'],correct: 1 }] }
+              ]
+            },
+            {
+              name: 'quiz3', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'],correct: 1 }] }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'React',
+          Quiz: [
+            {
+              name: 'quiz1', questions: [
+                {
+                  ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }],
+                  ques2: [{ ques: 'what is my name?', ans: ['Farjad', 'Noor', 'Eusufi'] }]
+                }
+              ]
+            },
+            {
+              name: 'quiz2', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }] }
+              ]
+            },
+            {
+              name: 'quiz3', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }] }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'PWA',
+          Quiz: [
+            {
+              name: 'quiz1', questions: [
+                {
+                  ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }],
+                  ques2: [{ ques: 'what is my name?', ans: ['Farjad', 'Noor', 'Eusufi'] }]
+                }
+              ]
+            },
+            {
+              name: 'quiz2', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }] }
+              ]
+            },
+            {
+              name: 'quiz3', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }] }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'HTML',
+          Quiz: [
+            {
+              name: 'quiz1', questions: [
+                {
+                  ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }],
+                  ques2: [{ ques: 'what is my name?', ans: ['Farjad', 'Noor', 'Eusufi'] }]
+                }
+              ]
+            },
+            {
+              name: 'quiz2', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }] }
+              ]
+            },
+            {
+              name: 'quiz3', questions: [
+                { ques1: [{ ques: 'what is your name?', ans: ['Farjad', 'Noor', 'Eusufi'] }] }
+              ]
+            }
+          ]
+        }
+      ]
+    };
 
-    this.switchOn = this.switchOn.bind(this);
-    this.switchOff = this.switchOff.bind(this);
-    this.breakBulb = this.breakBulb.bind(this);
+    this.enterQuiz = this.enterQuiz.bind(this);
+    this.enterCourse = this.enterCourse.bind(this);
+    this.showList = this.showList.bind(this);
+    this.login = this.login.bind(this);
 
   }
 
-  switchOn(){
-    this.setState({switch: false});
-    console.log('On***');
+  enterCourse(index) {
+    const { quizzes } = this.state;
+
+    this.setState({ quiz: quizzes[index],
+    course: true });
   }
 
-  switchOff(){
-    this.setState({switch: true});
-    console.log('Off***');
+  enterQuiz(index) {
+    const { quizzes } = this.state;
+    this.setState({ quiz: quizzes[index].Quiz[index], test: true });
   }
 
-  breakBulb(){
-    this.setState({bulb: true});
-    console.log('Break***');
+  showList() {
+    this.setState({ quiz: null });
   }
 
+  login() {
+    this.setState({ isLogin: true });
+    console.log('login**');
+  }
 
   render() {
-    // const { bulb, switch } = this.state;
+    const { quizzes, quiz, isLogin, course, prockey, test, startquiz } = this.state;
+
     return (
-      <div className="App">
-        {this.state.bulb && <Brokenbulb />}
-        {this.state.switch && !this.state.bulb && <Bulb switchOn={this.switchOn} breakBulb={this.breakBulb}/>}
-        {!this.state.switch && !this.state.bulb && <Lightbulb switchOff={this.switchOff} breakBulb={this.breakBulb}/>}
+      <div className="page">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title"> Quiz Panel</h1>
+          </header>
+        </div>
+        <div>
+          <QuizPage quiz={quiz} quiz1={quiz.Quiz} />
+          {/* {!isLogin && <Login onLogin={this.login} />}
+          {isLogin && !course && <CourseList list={quizzes} onPress={this.enterCourse} />}
+          {isLogin && course && !test && <QuizInfo quiz={quiz} quiz1={quiz.Quiz} onPress={this.enterQuiz} onBack={this.showList} />}
+          {isLogin && test && course && !prockey && <KeyPage / >} */}
+        </div>
       </div>
-    );
+
+    )
   }
+
 }
 
 export default App;
-
